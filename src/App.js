68750cd132f055c7aec7742b5cca67107c62c426
duyvox-dev <vobaoduy123@gmail.com>
2022-06-30 22:34:55 +0700

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { userRoutes } from "./routes/userRoutes";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    {userRoutes.map((route, index) => {
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={route.component}
+                            />
+                        );
+                    })}
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,7 @@
+export const userRoutes = [
+    // {
+    //     path: "/",
+    //     component: <LayoutTheme Component={HomePage} />,
+    //     exact: true,
+    // },
+];
