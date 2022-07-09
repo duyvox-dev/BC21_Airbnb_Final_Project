5978ab689d8b847da28c458037569e3650184266
duyvox-dev <vobaoduy123@gmail.com>
2022-07-09 09:45:58 +0700

    ini chitietphongslice

--- src/redux/chiTietPhongSlice.js
+++ src/redux/chiTietPhongSlice.js
@@ -1,16 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 const chiTietPhongSlice = createSlice({
     name: "chiTietPhongSlice",
-    initialState: {
-        daysOfBooking: 0,
-        location: "",
-        bookingDate: {
-            checkIn: "",
-            checkOut: "",
-        },
-        customerInfo: {},
-        isBookedSuccess: false,
-    },
+    initialState: {},
     reducers: {},
     extraReducers: {},
 });
