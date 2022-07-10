d439624e2808137970bf2782b56c2053b93b0ad4
riokez <riokez3377@gmail.com>
2022-07-10 17:59:32 +0700

    fix authSlice and max width modal

--- src/components/Modal/AuthModal.jsx
+++ src/components/Modal/AuthModal.jsx
@@ -4,13 +4,13 @@ import { Dialog } from "@headlessui/react";
 import FormDangNhap from "../../pages/DangNhapPage/FormDangNhap/FormDangNhap";
 import FormDangKy from "../../pages/DangKyPage/FormDangKy/FormDangKy";
 export default function AuthModal({ isModalOpen, toggleModal }) {
-    return (
-        <div>
-            <Modal isOpen={isModalOpen} onClose={toggleModal}>
-                <Dialog.Panel className="w-full md:max-w-[45rem] max-w-[25rem] transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all">
-                    <FormDangNhap></FormDangNhap>
-                </Dialog.Panel>
-            </Modal>
-        </div>
-    );
+  return (
+    <div>
+      <Modal isOpen={isModalOpen} onClose={toggleModal}>
+        <Dialog.Panel className="w-full md:max-w-[35rem] max-w-[25rem] transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all">
+          <FormDangNhap></FormDangNhap>
+        </Dialog.Panel>
+      </Modal>
+    </div>
+  );
 }
