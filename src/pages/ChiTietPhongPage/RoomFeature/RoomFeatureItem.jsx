a77ce94ebdfc8188ed93ffd5a1a1a7308f70d872
duyvox-dev <vobaoduy123@gmail.com>
2022-08-09 21:46:53 +0700

    Update RoomFeatureItem.jsx

--- src/pages/ChiTietPhongPage/RoomFeature/RoomFeatureItem.jsx
+++ src/pages/ChiTietPhongPage/RoomFeature/RoomFeatureItem.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 export default function RoomFeatureItem({ data }) {
-  return (
-    <div className="flex gap-2 items-center">
-      <img src={data.image} alt="" className="w-[30px] h-[30px]" />
-      <span className={data.disabled && "line-through"}>{data.name}</span>
-    </div>
-  );
+    return (
+        <div className="flex gap-2 items-center">
+            <img src={data.image} alt="" className="w-[30px] h-[30px]" />
+            <span className={`${data.disabled && "line-through"}`}>
+                {data.name}
+            </span>
+        </div>
+    );
 }
