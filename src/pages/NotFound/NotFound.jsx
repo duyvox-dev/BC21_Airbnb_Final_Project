498a94250b183c9738f57b48b5c25df2f4c933ec
Ngô Tuấn <ngotuan@admins-MacBook-Air.local>
2022-08-09 19:06:06 +0700

    page notfound

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
-  return (
-    <div>NotFound</div>
-  )
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    document.title = '404 Not Found';
+
+    return (
+        <div className="container flex items-center justify-start min-h-full mx-auto text-2xl font-bold py-36">
+            <span>Không tìm thấy trang, vui lòng quay lại </span>
+            <Link
+                className="pl-1 hover:underline text-[#ff5a5e] hover:text-[#ff5a5e]"
+                to={'/'}
+            >
+                trang chủ.
+            </Link>
+        </div>
+    );
 }
