618e3ac06ff22baadb595b9f3b997f7a50762518
Ngô Tuấn <ngotuan@admins-MacBook-Air.local>
2022-08-09 15:29:33 +0700

    fix footer

--- src/HOC/ThemeLayout/FooterTemplate/FooterTemplate.jsx
+++ src/HOC/ThemeLayout/FooterTemplate/FooterTemplate.jsx
@@ -10,99 +10,101 @@ import styles from '../css/FooterTemplate.css';
 
 export default function FooterTemplate() {
     return (
-        <div className="hidden w-full footer bg-neutral-100 md:block">
-            <div className="w-11/12 py-5 mx-auto">
-                <div className="grid w-full grid-cols-1 pb-2 mx-auto border-0 border-b border-solid xl:gap-10 xl:grid-cols-4 justify-items-start border-b-neutral-300">
-                    <div className="w-full pb-2 border-b xl:border-0">
-                        <h3 className="pb-3 text-sm font-bold text-left">
-                            Hỗ trợ
-                        </h3>
-                        <div className="grid w-full grid-cols-3 xl:grid-cols-1">
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                Trung tâm trợ giúp
-                            </p>
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                AirCover
-                            </p>
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                Thông tin an toàn
-                            </p>
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                Hỗ trợ người khuyết tật
-                            </p>
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                Các tuỳ chọn huỷ
-                            </p>
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                Biện pháp ứng phó với đại dịch COVID-19 của
-                                chúng tôi
-                            </p>
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                Báo cáo lo ngại của hàng xóm
-                            </p>
+        <div className="w-full footer bg-neutral-100">
+            <div className="container py-5 mx-auto">
+                <div className="hidden px-4 md:block xl:px-0">
+                    <div className="grid w-full grid-cols-1 pb-2 mx-auto border-0 border-b border-solid xl:gap-10 xl:grid-cols-4 justify-items-start border-b-neutral-300">
+                        <div className="w-full pb-2 border-b xl:border-0">
+                            <h3 className="pb-3 text-sm font-bold text-left">
+                                Hỗ trợ
+                            </h3>
+                            <div className="grid w-full grid-cols-3 xl:grid-cols-1">
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Trung tâm trợ giúp
+                                </p>
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    AirCover
+                                </p>
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Thông tin an toàn
+                                </p>
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Hỗ trợ người khuyết tật
+                                </p>
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Các tuỳ chọn huỷ
+                                </p>
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Biện pháp ứng phó với đại dịch COVID-19 của
+                                    chúng tôi
+                                </p>
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Báo cáo lo ngại của hàng xóm
+                                </p>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="w-full pt-4 pb-2 border-b xl:border-0">
-                        <h3 className="pb-3 text-sm font-bold text-left">
-                            Cộng đồng
-                        </h3>
-                        <div className="grid w-full grid-cols-3 xl:grid-cols-1">
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                Airbnb.org: nhà ở cứu trợ
-                            </p>
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                Hỗ trợ dân tị nạn Afghanistan
-                            </p>
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                Chống phân biệt đối xử
-                            </p>
+                        <div className="w-full pt-4 pb-2 border-b xl:border-0">
+                            <h3 className="pb-3 text-sm font-bold text-left">
+                                Cộng đồng
+                            </h3>
+                            <div className="grid w-full grid-cols-3 xl:grid-cols-1">
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Airbnb.org: nhà ở cứu trợ
+                                </p>
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Hỗ trợ dân tị nạn Afghanistan
+                                </p>
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Chống phân biệt đối xử
+                                </p>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="w-full pt-4 pb-2 border-b xl:border-0">
-                        <h3 className="pb-3 text-sm font-bold text-left">
-                            Đón tiếp khách
-                        </h3>
-                        <div className="grid w-full grid-cols-3 xl:grid-cols-1">
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                Thử đón tiếp khách
-                            </p>
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                AirCover cho Chủ nhà
-                            </p>
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                Xem tài nguyên đón tiếp khách
-                            </p>
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                Truy cập diễn đàn cộng đồng
-                            </p>
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                Đón tiếp khách có trách nhiệm
-                            </p>
+                        <div className="w-full pt-4 pb-2 border-b xl:border-0">
+                            <h3 className="pb-3 text-sm font-bold text-left">
+                                Đón tiếp khách
+                            </h3>
+                            <div className="grid w-full grid-cols-3 xl:grid-cols-1">
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Thử đón tiếp khách
+                                </p>
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    AirCover cho Chủ nhà
+                                </p>
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Xem tài nguyên đón tiếp khách
+                                </p>
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Truy cập diễn đàn cộng đồng
+                                </p>
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Đón tiếp khách có trách nhiệm
+                                </p>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="w-full pt-4">
-                        <h3 className="pb-3 text-sm font-bold text-left">
-                            Airbnb
-                        </h3>
-                        <div className="grid w-full grid-cols-3 xl:grid-cols-1">
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                Trang tin tức
-                            </p>
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                Tìm hiểu các tính năng mới
-                            </p>
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                Thư ngõ từ các nhà sáng lập
-                            </p>
-                            <p className="text-sm text-left cursor-pointer hover:underlined">
-                                Cơ hội nghề nghiệp
-                            </p>
-                            <p className="text-sm text-left cursor-pointer hover:underline">
-                                Nhà đầu tư
-                            </p>
+                        <div className="w-full pt-4">
+                            <h3 className="pb-3 text-sm font-bold text-left">
+                                Airbnb
+                            </h3>
+                            <div className="grid w-full grid-cols-3 xl:grid-cols-1">
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Trang tin tức
+                                </p>
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Tìm hiểu các tính năng mới
+                                </p>
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Thư ngõ từ các nhà sáng lập
+                                </p>
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Cơ hội nghề nghiệp
+                                </p>
+                                <p className="text-sm text-left cursor-pointer hover:underline">
+                                    Nhà đầu tư
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -112,13 +114,13 @@ export default function FooterTemplate() {
                             2021 AirBnb, Inc. All rights reserved.
                         </p>
                         <div className="flex justify-center space-x-2 xl:ml-5 xl:space-x-4">
-                            <p className="mx-1 my-auto text-sm md:mx-0 md:my-0 lg:mx-0 lg:my-0">
+                            <p className="mx-1 my-auto text-sm cursor-pointer md:mx-0 md:my-0 lg:mx-0 lg:my-0 hover:underline">
                                 Quyền riêng tư
                             </p>
-                            <p className="mx-1 my-auto text-sm md:mx-0 md:my-0 lg:mx-0 lg:my-0">
+                            <p className="mx-1 my-auto text-sm cursor-pointer md:mx-0 md:my-0 lg:mx-0 lg:my-0 hover:underline">
                                 Điều khoản
                             </p>
-                            <p className="mx-1 my-auto text-sm md:mx-0 md:my-0 lg:mx-0 lg:my-0">
+                            <p className="mx-1 my-auto text-sm cursor-pointer md:mx-0 md:my-0 lg:mx-0 lg:my-0 hover:underline">
                                 Sơ đồ trang web
                             </p>
                         </div>
@@ -128,14 +130,14 @@ export default function FooterTemplate() {
                             className="my-auto lg:my-0"
                             icon={faGlobe}
                         />
-                        <p className="mx-1 my-auto text-sm font-medium md:text-center lg:my-0">
+                        <p className="mx-1 my-auto text-sm font-medium cursor-pointer md:text-center lg:my-0 hover:underline">
                             Tiếng Việt (VN)
                         </p>
                         <FontAwesomeIcon
                             className="mx-1 my-auto lg:my-0"
                             icon={faDongSign}
                         />
-                        <p className="mx-1 my-auto text-sm font-medium lg:my-0">
+                        <p className="mx-1 my-auto text-sm font-medium cursor-pointer lg:my-0 hover:underline">
                             VND
                         </p>
                         <div className="ml-5 space-x-2">
--- src/pages/DanhSachPhongPage/ModalChonPhong.jsx
+++ src/pages/DanhSachPhongPage/ModalChonPhong.jsx
@@ -1,82 +1,88 @@
-import React, { useState, Fragment } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRightArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import { Dialog, Transition } from "@headlessui/react";
+import React, { useState, Fragment } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { Dialog, Transition } from '@headlessui/react';
 
-import KhoangGia from "./KhoangGia";
-import PhongNgu from "./PhongNgu";
-import TienNghi from "./TienNghi";
-import Modal from "../../components/Modal/Modal";
+import KhoangGia from './KhoangGia';
+import PhongNgu from './PhongNgu';
+import TienNghi from './TienNghi';
+import Modal from '../../components/Modal/Modal';
 import {
-  deleteAllSearchRoomList,
-  handleSearchRoomList,
-} from "../../redux/danhSachPhongSlice";
+    deleteAllSearchRoomList,
+    handleSearchRoomList,
+} from '../../redux/danhSachPhongSlice';
 
 export default function ModalChonPhong() {
-  const { danhSachPhong } = useSelector((state) => state.danhSachPhongSlice);
-  const [isOpen, setIsOpen] = useState(false);
-  const dispatch = useDispatch();
+    const { danhSachPhong } = useSelector((state) => state.danhSachPhongSlice);
+    const [isOpen, setIsOpen] = useState(false);
+    const dispatch = useDispatch();
 
-  let onClose = () => {
-    setIsOpen(false);
-  };
+    let onClose = () => {
+        setIsOpen(false);
+    };
 
-  return (
-    <>
-      <div className="fixed z-10 w-full py-5 text-center bg-white border-b top-[8rem] sm:top-[5.7rem]">
-        <div className="flex items-center justify-center space-x-32">
-          <div className="text-lg font-medium">
-            Hơn {danhSachPhong?.length} chỗ ở
-          </div>
-          <button
-            onClick={() => setIsOpen(true)}
-            className="flex items-center justify-center px-4 py-2 space-x-2 border rounded-lg"
-          >
-            <FontAwesomeIcon icon={faArrowRightArrowLeft} />
-            <div>Bộ lọc</div>
-          </button>
-          <Modal
-            isOpen={isOpen}
-            onClose={onClose}
-            children={
-              <Dialog.Panel className="w-full h-[80%] md:max-w-[35rem] lg:max-w-[45rem]  max-w-[80%] transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all relative">
-                <div className="fixed top-0 z-10 w-full p-5 text-lg font-medium text-center text-gray-900 bg-white border-b">
-                  Bộ lọc
-                </div>
-                <div className="py-20 h-[100%] overflow-y-scroll">
-                  <div className="p-5">
-                    <KhoangGia />
-                    <PhongNgu />
-                    <TienNghi />
-                  </div>
-                </div>
-
-                <div className="fixed bottom-0 z-10 w-full px-5 py-4 bg-white border-t">
-                  <div className="flex justify-between">
+    return (
+        <>
+            <div className="fixed z-10 w-full py-5 text-center bg-white border-b top-[8rem] sm:top-[5.5rem]">
+                <div className="flex items-center justify-center space-x-32">
+                    <div className="text-lg font-medium">
+                        Hơn {danhSachPhong?.length} chỗ ở
+                    </div>
                     <button
-                      className="px-2 text-lg font-medium underline transition-all duration-300 rounded-lg hover:bg-stone-100"
-                      onClick={() => dispatch(deleteAllSearchRoomList())}
+                        onClick={() => setIsOpen(true)}
+                        className="flex items-center justify-center px-4 py-2 space-x-2 border rounded-lg"
                     >
-                      Xóa tất cả
+                        <FontAwesomeIcon icon={faArrowRightArrowLeft} />
+                        <div>Bộ lọc</div>
                     </button>
-                    <button
-                      type="button"
-                      className="inline-flex justify-center px-4 py-2 text-base font-medium text-white transition-all duration-300 border border-transparent rounded-md bg-stone-700 hover:bg-stone-900"
-                      onClick={() => {
-                        setIsOpen(false);
-                        dispatch(handleSearchRoomList());
-                      }}
-                    >
-                      Tìm kiếm
-                    </button>
-                  </div>
+                    <Modal
+                        isOpen={isOpen}
+                        onClose={onClose}
+                        children={
+                            <Dialog.Panel className="w-full h-[80%] md:max-w-[35rem] lg:max-w-[45rem]  max-w-[80%] transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all relative">
+                                <div className="fixed top-0 z-10 w-full p-5 text-lg font-medium text-center text-gray-900 bg-white border-b">
+                                    Bộ lọc
+                                </div>
+                                <div className="py-20 h-[100%] overflow-y-scroll">
+                                    <div className="p-5">
+                                        <KhoangGia />
+                                        <PhongNgu />
+                                        <TienNghi />
+                                    </div>
+                                </div>
+
+                                <div className="fixed bottom-0 z-10 w-full px-5 py-4 bg-white border-t">
+                                    <div className="flex justify-between">
+                                        <button
+                                            className="px-2 text-lg font-medium underline transition-all duration-300 rounded-lg hover:bg-stone-100"
+                                            onClick={() =>
+                                                dispatch(
+                                                    deleteAllSearchRoomList()
+                                                )
+                                            }
+                                        >
+                                            Xóa tất cả
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className="inline-flex justify-center px-4 py-2 text-base font-medium text-white transition-all duration-300 border border-transparent rounded-md bg-stone-700 hover:bg-stone-900"
+                                            onClick={() => {
+                                                setIsOpen(false);
+                                                dispatch(
+                                                    handleSearchRoomList()
+                                                );
+                                            }}
+                                        >
+                                            Tìm kiếm
+                                        </button>
+                                    </div>
+                                </div>
+                            </Dialog.Panel>
+                        }
+                    />
                 </div>
-              </Dialog.Panel>
-            }
-          />
-        </div>
-      </div>
-    </>
-  );
+            </div>
+        </>
+    );
 }
